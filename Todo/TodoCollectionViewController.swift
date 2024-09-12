8d167398f5a1a46d0de1fb0c4d733ca8fa7b068d
HisadaYuhei <hsdy20010918@gmail.com>
2024-09-12 15:47:25 +0900

    提出版

--- Todo/TodoCollectionViewController.swift
+++ Todo/TodoCollectionViewController.swift
@@ -1,29 +1,52 @@
-//
-//  TodoCollectionViewController.swift
-//  Todo
-//
-//  Created by 久田　悠平 on 2024/09/12.
-//
-
 import UIKit
 
-class TodoCollectionViewController: UIViewController {
+class TodoCollectionViewController: UIViewController, UICollectionViewDataSource {
+    @IBOutlet var collectionView: UICollectionView!
+    
+    var saveData: UserDefaults = UserDefaults.standard
+    
+    var titles: [String] = []
+    var contents: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // UserDefaultsにデフォルト値を登録
+        saveData.register(defaults: ["titles": [], "contents": []])
+
+        // UserDefaultsからデータを安全に取得
+        if let savedTitles = saveData.object(forKey: "titles") as? [String] {
+            titles = savedTitles
+        }
+        
+        if let savedContents = saveData.object(forKey: "contents") as? [String] {
+            contents = savedContents
+        }
+
+        collectionView.dataSource = self
+
+        // レイアウト設定
+        let configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+        collectionView.collectionViewLayout = UICollectionViewCompositionalLayout.list(using: configuration)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // セクション内のアイテム数を返す
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return titles.count
     }
-    */
+    
+    // セルの内容を設定
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
 
+        // UIListContentConfigurationでセルのテキストを設定
+        var contentConfiguration = UIListContentConfiguration.cell()
+        contentConfiguration.text = titles[indexPath.item]
+        contentConfiguration.secondaryText = contents[indexPath.item]
+        
+        // セルにコンテンツを設定
+        cell.contentConfiguration = contentConfiguration
+        
+        return cell
+    }
 }
--- Todo/TodoViewController.swift
+++ Todo/TodoViewController.swift
@@ -1,29 +1,61 @@
-//
-//  TodoViewController.swift
-//  Todo
-//
-//  Created by 久田　悠平 on 2024/09/12.
-//
-
 import UIKit
 
-class TodoViewController: UIViewController {
+class TodoViewController: UIViewController, UITextFieldDelegate{
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentTextField: UITextField!
+    
+    var saveData: UserDefaults = UserDefaults.standard
+    
+    var titles: [String] = []
+    var contents: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // デフォルトの値を登録
+        saveData.register(defaults: ["titles": [], "contents": []])
+        
+        // UserDefaultsから保存されたデータを読み込む（安全にキャスト）
+        if let savedTitles = saveData.object(forKey: "titles") as? [String] {
+            titles = savedTitles
+        }
+        
+        if let savedContents = saveData.object(forKey: "contents") as? [String] {
+            contents = savedContents
+        }
 
-        // Do any additional setup after loading the view.
+        titleTextField.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder() // キーボードを閉じる
+        return true
     }
-    */
-
+    
+    @IBAction func save(_ sender: Any) {
+        guard let title = titleTextField.text, !title.isEmpty,
+              let content = contentTextField.text, !content.isEmpty else {
+            // タイトルまたは内容が空の場合、エラーメッセージを表示
+            let alert = UIAlertController(title: "エラー", message: "タイトルまたは内容が空です。", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        // タイトルと内容を配列に追加
+        titles.append(title)
+        contents.append(content)
+        
+        // UserDefaultsに保存（配列全体を保存）
+        saveData.set(titles, forKey: "titles")
+        saveData.set(contents, forKey: "contents")
+        
+        // 保存完了メッセージの表示
+        let alert: UIAlertController = UIAlertController(title: "追加", message: "Todoの追加が完了しました。", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            // 保存後に前の画面に戻る
+            self.navigationController?.popViewController(animated: true)
+        }))
+        present(alert, animated: true, completion: nil)
+    }
 }
